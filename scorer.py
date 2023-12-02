776d02c9ab486ac86af390c88360541e5d754a21
joshuahahn <joshua.hahnjy@gmail.com>
2023-12-02 11:47:22 -0500

    merging

--- scorer.py
+++ scorer.py
@@ -22,16 +22,16 @@ def win(state):
     # Check horizontal & vertical wins
     for i in range(3):
         if state[i][0] != 0 and state[i][0] == state[i][1] == state[i][2]:
-            return 3 - (2 * state[i][0])
+            return state[i][0]
         if state[0][i] != 0 and state[0][i] == state[1][i] == state[2][i]:
-            return 3 - (2 * state[i][0])
+            return state[i][0]
 
     # Check diagonal wins
     if state[0][0] != 0 and state[0][0] == state[1][1] == state[2][2]:
-        return 3 - (2 * state[0][0])
+        return state[0][0]
 
     if state[0][2] != 0 and state[0][2] == state[1][1] == state[2][0]:
-        return 3 - (2 * state[0][2])
+        return state[0][0]
 
     # No winner yet
     return 0
@@ -52,7 +52,7 @@ def draw(state):
     return True
 
 def score(state):
-    """ Returns who is winning """
+    """ Returns who is winning. 0: tied, 1: X is winning, 2: O is winning """
 
     # First check if the game is already over
     res = win(state)
@@ -62,28 +62,11 @@ def score(state):
     # We'll be using a few heuristics to meausre the state of the game,
     # with weight assigned in the following order:
     # 1. Number of rows / diagonals / columns with 2 claimed cells and
-    #    an empty slot in the remaining cell (+/- 10)
+    #    an empty slot in the remaining cell. If there is such a row and
+    #    it is that player's turn, we return that they are winning.
     # 2. Number of rows / diagonals / colums with 1 claimed cell and
-    #    two empty slots in the remaining cells (+/- 5)
-    # 3. Who's turn it is (+/- 5)
-
-    lines = []
-    for i in range(3):
-        lines.append(set([state[0][i], state[1][i], state[2][i]]))
-        lines.append(set([state[i][0], state[i][1], state[i][2]]))
-
-    lines.append(set([state[0][0], state[1][1], state[2][2]]))
-    lines.append(set([state[0][2], state[1][1], state[2][0]]))
-
-    for line in lines:
-        if 0 in line:
-            lineSum = sum(line)
-            if 1 in line:
-                if lineSum == 2: res += 10  # 2 in a line, O
-                elif lineSum == 1: res += 5 # 1 in a line, O
-            if 2 in line:
-                if lineSum == 4: res -= 10  # 2 in a line, X
-                elif lineSum == 2: res -= 5 # 1 in a line, X
+    #    two empty slots in the remaining cells
+    # 3. Who's turn it is
 
     tileCount = 0
     for row in state:
@@ -94,13 +77,38 @@ def score(state):
                 tileCount -= 1
 
     if tileCount == 0:
-        res -= 5 # X's turn
+        turn = "X"
     else:
-        res += 5 # O's turn
+        turn = "O"
+
+    lines = []
+    for i in range(3):
+        lines.append(([state[0][i], state[1][i], state[2][i]]))
+        lines.append(([state[i][0], state[i][1], state[i][2]]))
+
+    lines.append(([state[0][0], state[1][1], state[2][2]]))
+    lines.append(([state[0][2], state[1][1], state[2][0]]))
+    
+    for line in lines:
+        if 0 in line:
+            lineSum = sum(line)
+            if 1 in line:
+                if lineSum == 2: 
+                    if turn == 'O':
+                        return 2
+                    res += 10  # 2 in a line, O
+                elif lineSum == 1: res += 5 # 1 in a line, O
+            if 2 in line:
+                if lineSum == 4:
+                    if turn == 'X':
+                        return 1
+                    
+                    res -= 10  # 2 in a line, X
+                elif lineSum == 2: res -= 5 # 1 in a line, X
 
     if abs(res) <= 5:
         return 0
     elif res > 5:
-        return 1
+        return 2
     else:
-        return -1
+        return 1
